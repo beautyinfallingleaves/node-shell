93f693bcbe4377713a433aa3803d77119defcfc0
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-09-23 11:38:41 -0500

    add ls

--- bash.js
+++ bash.js
@@ -1,17 +1,17 @@
 // Output a prompt
-process.stdout.write('prompt > ');
+//process.stdout.write('> ');
 
 // The stdin 'data' event fires aftter a user types in a line
 process.stdin.on('data', (input) => {
   const cmd = input.toString().trim(); // remove the newline
   if (cmd === 'pwd') {
     require('./pwd');
-  } else if (cmd === 'ls') {
-
   }
-
+  else if (cmd === 'ls') {
+    require('./ls');
+  }
   else {
     process.stdout.write('You typed: ' + cmd);
   }
-  process.stdout.write('\nprompt > ');
+
 });
